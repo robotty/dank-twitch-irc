dde0fe5859409ef6acae9b08892935a8336810ca
Ruben Anders <ruben.anders@robotty.de>
2019-05-27 13:53:25 +0200

    Refactor badge parser slightly

--- lib/message/parser/badges.ts
+++ lib/message/parser/badges.ts
@@ -1,19 +1,27 @@
-import {TwitchBadge, TwitchBadgesList} from '../badges';
+import { TwitchBadge, TwitchBadgesList } from '../badges';
+
+function parseSingleBadge(badgeSrc: string): TwitchBadge {
+    // src format: <badge>/<version>
+
+    let [badgeName, badgeVersionSrc] = badgeSrc.split('/', 2);
+    if (typeof badgeName === 'undefined' || typeof badgeVersionSrc === 'undefined') {
+        throw new Error('Invalid badge source: ' + badgeSrc);
+    }
+
+    let badgeVersion = parseInt(badgeVersionSrc);
+    if (isNaN(badgeVersion)) {
+        throw new Error('Unparseable badge version string: ' + badgeVersionSrc);
+    }
+
+    return new TwitchBadge(badgeName, badgeVersion);
+}
 
 export function parseBadges(badgesSrc: string): TwitchBadgesList {
+    // src format: <badge>/<version>,<badge>/<version>,<badge>/<version>
+
     let badges = new TwitchBadgesList();
     for (let badgeSrc of badgesSrc.split(',')) {
-        let [badgeName, badgeVersionSrc] = badgeSrc.split('/', 2);
-        if (typeof badgeName === 'undefined' || typeof badgeVersionSrc === 'undefined') {
-            continue;
-        }
-
-        let badgeVersion = parseInt(badgeVersionSrc);
-        if (isNaN(badgeVersion)) {
-            continue;
-        }
-
-        badges.push(new TwitchBadge(badgeName, badgeVersion));
+        badges.push(parseSingleBadge(badgeSrc));
     }
     return badges;
 }
