21372c2ad81cfe7ac88b9e659ca5664efec56fb6
Ruben Anders <ruben.anders@robotty.de>
2019-08-13 23:04:10 +0200

    Try out some coverage and test report related things

--- .mocharc.js
+++ .mocharc.js
@@ -1,8 +1,8 @@
 module.exports = {
   require: ["ts-node/register", "source-map-support/register"],
-  reporter: "mochawesome",
+  reporter: ["mochawesome", "mocha-junit-reporter"],
+  reporterOptions: "output=./mocha-report/test-results.xml",
   recursive: true,
   spec: "lib/**/*.spec.ts",
-  extension: [".spec.ts"],
-  timeout: 10000
+  extension: [".spec.ts"]
 };
