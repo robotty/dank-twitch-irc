8a7e02ff12a73317fc43dd322b9ea4da5d9eae56
Ruben Anders <ruben.anders@robotty.de>
2019-08-13 23:28:18 +0200

    Finalize it i think

--- .mocharc.js
+++ .mocharc.js
@@ -1,7 +1,6 @@
 module.exports = {
   require: ["ts-node/register", "source-map-support/register"],
-  reporter: ["mochawesome", "mocha-junit-reporter"],
-  reporterOptions: "output=./mocha-report/test-results.xml",
+  reporters: "mochawesome",
   recursive: true,
   spec: "lib/**/*.spec.ts",
   extension: [".spec.ts"]
