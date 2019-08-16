17c3ced0efb3228ea492eb124fc6417a0b627624
Ruben Anders <ruben.anders@robotty.de>
2019-08-17 00:06:33 +0200

    Fix secure TCP transport not connecting

--- lib/client/transport/tcp-transport.ts
+++ lib/client/transport/tcp-transport.ts
@@ -5,23 +5,32 @@ import { Transport } from "./transport";
 
 export class TcpTransport implements Transport {
   public readonly stream: Socket | TLSSocket;
+  private readonly backingSocket?: Socket;
   private readonly config: ExpandedTcpTransportConfiguration;
 
   public constructor(config: ExpandedTcpTransportConfiguration) {
     this.config = config;
 
     if (config.secure) {
-      this.stream = new TLSSocket(new Socket());
+      this.backingSocket = new Socket();
+      this.backingSocket.setNoDelay(true);
+      this.backingSocket.setDefaultEncoding("utf-8");
+
+      this.stream = new TLSSocket(this.backingSocket);
     } else {
       this.stream = new Socket();
+      this.stream.setNoDelay(true);
+      this.stream.setDefaultEncoding("utf-8");
     }
 
-    this.stream.setNoDelay(true);
-    this.stream.setDefaultEncoding("utf-8");
     this.stream.cork();
   }
 
   public connect(connectionListener?: () => void): void {
+    if (this.backingSocket != null) {
+      this.backingSocket.connect(this.config.port, this.config.host);
+    }
+
     this.stream.connect(this.config.port, this.config.host, () => {
       this.stream.uncork();
       if (connectionListener != null) {
