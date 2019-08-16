c578e6bf6032b0d75a58f3a2fb83cca4e1abe2d6
Ruben Anders <ruben.anders@robotty.de>
2019-08-16 14:52:51 +0200

    Fix websocket transport

--- lib/client/transport/websocket-transport.ts
+++ lib/client/transport/websocket-transport.ts
@@ -1,32 +1,40 @@
 import { Duplexify } from "duplexify";
 import * as duplexify from "duplexify";
-import * as WebSocketStream from "websocket-stream";
-import { WebSocketDuplex } from "websocket-stream";
+import * as WebSocketDuplex from "simple-websocket";
+import { PassThrough } from "stream";
 import { ExpandedWebSocketTransportConfiguration } from "../../config/expanded";
 import { Transport } from "./transport";
 
 export class WebSocketTransport implements Transport {
   public readonly stream: Duplexify;
+  private readonly readable: PassThrough;
+  private readonly writable: PassThrough;
+
   private readonly config: ExpandedWebSocketTransportConfiguration;
   private wsStream: WebSocketDuplex | undefined;
 
   public constructor(config: ExpandedWebSocketTransportConfiguration) {
     this.config = config;
 
-    // A duplexify is "corked" by default
-    // the write buffer will be forwarded to the websocket stream
-    // via the setWritable call in connect()
-    this.stream = duplexify();
+    this.readable = new PassThrough({ decodeStrings: false, objectMode: true });
+    this.writable = new PassThrough({ decodeStrings: false, objectMode: true });
+    this.stream = duplexify(this.writable, this.readable, {
+      decodeStrings: false,
+      objectMode: true
+    });
   }
 
   public connect(connectionListener?: () => void): void {
-    this.wsStream = WebSocketStream(this.config.url);
+    this.wsStream = new WebSocketDuplex({
+      url: this.config.url,
+      decodeStrings: false,
+      objectMode: true
+    });
     if (connectionListener != null) {
       this.wsStream.once("connect", connectionListener);
     }
 
-    // uncork
-    this.stream.setReadable(this.wsStream);
-    this.stream.setWritable(this.wsStream);
+    this.wsStream.pipe(this.readable);
+    this.writable.pipe(this.wsStream);
   }
 }
