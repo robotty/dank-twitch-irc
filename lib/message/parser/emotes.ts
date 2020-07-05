4aae845bf0d0c5d3f0dd71b7a0a5d67bf8f903b5
Karar Al-Remahy <alremahykarar@ymail.com>
2020-07-05 18:08:01 +0200

    Implemented emoji counter to potentially fix #22 & #35

--- lib/message/parser/emotes.ts
+++ lib/message/parser/emotes.ts
@@ -13,10 +13,24 @@ export function parseEmotes(
     return emotes;
   }
 
+  // Gets emojis in text
+  const emojis = [];
+
+  // Lodash library regexp for matching emojis.
+  const regex = /(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff])[\ufe0e\ufe0f]?(?:[\u0300-\u036f\ufe20-\ufe23\u20d0-\u20f0]|\ud83c[\udffb-\udfff])?(?:\u200d(?:[^\ud800-\udfff]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff])[\ufe0e\ufe0f]?(?:[\u0300-\u036f\ufe20-\ufe23\u20d0-\u20f0]|\ud83c[\udffb-\udfff])?)*/g;
+  const matchedEmojis = [...messageText.matchAll(regex)];
+  for (let index = 0; index < matchedEmojis.length; index++) {
+    const emoji = matchedEmojis[index];
+    if (emoji !== null && emoji.index !== undefined) {
+      emojis.push(emoji.index);
+    }
+  }
+
   for (const emoteInstancesSrc of emotesSrc.split("/")) {
     const [emoteID, instancesSrc] = emoteInstancesSrc.split(":", 2);
+    let emojiCount = 0
     for (const instanceSrc of instancesSrc.split(",")) {
-      const [startIndex, endIndexInclusive] = instanceSrc
+      let [startIndex, endIndexInclusive] = instanceSrc
         .split("-")
         .map(parseIntThrowing);
       if (endIndexInclusive == null) {
@@ -24,6 +38,11 @@ export function parseEmotes(
           `No - found in emote index range "${instanceSrc}"`
         );
       }
+
+      // Fix for when emojis exist before this emote.
+      emojiCount = emojis.filter(emojiIndex => emojiIndex <= (startIndex + (emojiCount > 0 ? emojiCount - 1 : 0))).length;
+      startIndex += emojiCount;
+      endIndexInclusive += emojiCount;
 
       // to make endIndex exclusive
       const endIndex = endIndexInclusive + 1;
